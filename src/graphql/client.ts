0f16439829b4c2a94be461180275a4d580c785a3
Gabriel-Amorim-Ortiz <contato.ortizgabriel.com>
2023-07-24 09:54:04 -0300

    Refatorando cliente GRAPHQL

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,10 +2,10 @@ import { GraphQLClient } from "graphql-request";
 
 
 const endpoint = process.env.GRAPHQL_HOST || ''
-
+const token = process.env.GRAPHQL_TOKEN ||''
 const client = new GraphQLClient(endpoint,{
  headers: {
-  authorization:`Bearer ${process.env.GRAPHQL_TOKEN}`
+  authorization:`Bearer ${token}`
  }
  })
 
